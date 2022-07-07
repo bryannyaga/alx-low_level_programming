c9354dab164ec3660c5c47ad3dbd387554a5372c
BryanNyaga <bryanributhu@gmail.com>
2022-07-07 08:34:51 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+int i;
+int j;
+int k;
+for (i = 48; i <= 58; i++)
+{
+for (j = 1; j <= 58; j++)
+{
+for (k = j; k < 58; k++)
+{
+if (i == j || j == k || i == k)
+{
+continue;
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (i == 55 && j == 56 && k == 57)
+{
+break;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar ('\n');
+return  (0);
+}
+}
+}
